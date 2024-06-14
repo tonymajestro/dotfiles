a1a82087b451d278d4f414584b0d1068d1852b05
Tony Majestro <majestro@amazon.com>
2024-06-14 15:26:40 -0700

    Remove git from p10k

--- p10k/.p10k.zsh
+++ p10k/.p10k.zsh
@@ -52,7 +52,6 @@
     # =========================[ Line #1 ]=========================
     # context                 # user@host
     dir                       # current directory
-    vcs                       # git status
     # command_execution_time  # previous command duration
     # =========================[ Line #2 ]=========================
     newline                   # \n
