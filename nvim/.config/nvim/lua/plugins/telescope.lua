b1a24677d7d6063913929a9ccf8fe5340d4402ee
Tony Majestro <majestro@amazon.com>
2024-07-09 14:35:49 -0700

    Add preview keymap and smart paths for Telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -2,11 +2,15 @@ return {
   "nvim-telescope/telescope.nvim",
   opts = function(_, opts)
     local actions = require("telescope.actions")
+    local layout = require("telescope.actions.layout")
     opts.defaults.mappings.i = vim.tbl_extend("force", opts.defaults.mappings.i, {
       ["<esc>"] = actions.close, -- close Telescope window with escape
       ["<C-k>"] = actions.move_selection_previous, -- move to prev result
       ["<C-j>"] = actions.move_selection_next, -- move to next result
+      ["<C-p>"] = layout.toggle_preview,
     })
+
+    opts.defaults.path_display = { "smart" }
   end,
   keys = {
     { "<leader>fg", "<cmd>Telescope git_status<CR>", desc = "Telescope git status" },
