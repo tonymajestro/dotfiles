8410f7ee241e67a69c5f0d371b7f6b9b3556ba59
Tony Majestro <majestro@amazon.com>
2024-07-15 11:33:18 -0700

    Simplify dashboard

---
+++ nvim/.config/nvim/lua/plugins/dashboard.lua
@@ -0,0 +1,41 @@
+return {
+  "nvimdev/dashboard-nvim",
+  opts = {
+    config = {
+      center = {
+        {
+          action = "lua LazyVim.pick()()",
+          desc = " Find File",
+          icon = " ",
+          key = "f",
+        },
+        {
+          action = "ene | startinsert",
+          desc = " New File",
+          icon = " ",
+          key = "n",
+        },
+        {
+          action = 'lua require("persistence").load()',
+          desc = " Restore Session",
+          icon = " ",
+          key = "s",
+        },
+        {
+          action = "Lazy",
+          desc = " Lazy",
+          icon = "󰒲 ",
+          key = "l",
+        },
+        {
+          action = function()
+            vim.api.nvim_input("<cmd>qa<cr>")
+          end,
+          desc = " Quit",
+          icon = " ",
+          key = "q",
+        },
+      },
+    },
+  },
+}
