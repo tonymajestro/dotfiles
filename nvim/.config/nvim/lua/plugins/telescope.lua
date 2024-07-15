8626b77b24afe1d8d79f9a968e7a1003510ad779
Tony Majestro <tonymajestro@gmail.com>
2024-07-14 22:52:49 -0700

    Move Telescope prompt to top of screen

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,16 +1,26 @@
 return {
   "nvim-telescope/telescope.nvim",
-  opts = function(_, opts)
+  opts = function()
     local actions = require("telescope.actions")
     local layout = require("telescope.actions.layout")
-    opts.defaults.mappings.i = vim.tbl_extend("force", opts.defaults.mappings.i, {
-      ["<esc>"] = actions.close, -- close Telescope window with escape
-      ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-      ["<C-j>"] = actions.move_selection_next, -- move to next result
-      ["<C-p>"] = layout.toggle_preview,
-    })
 
-    opts.defaults.path_display = { "smart" }
+    return {
+      defaults = {
+        layout_config = {
+          prompt_position = "top",
+        },
+        sorting_strategy = "ascending",
+        mappings = {
+          i = {
+            ["<esc>"] = actions.close, -- close Telescope window with escape
+            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+            ["<C-j>"] = actions.move_selection_next, -- move to next result
+            ["<C-p>"] = layout.toggle_preview,
+          },
+        },
+        path_display = { "smart " },
+      },
+    }
   end,
   keys = {
     { "<leader>fg", "<cmd>Telescope git_status<CR>", desc = "Telescope git status" },
