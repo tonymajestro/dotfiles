8d1f025bc05d8d00f27e0b0e8f274ccb08266563
Tony Majestro <majestro@amazon.com>
2024-08-01 10:42:09 -0700

    Remove ligatures from wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,7 +4,7 @@ local config = wezterm.config_builder()
 
 config.color_scheme = "Catppuccin Macchiato"
 config.font_size = 22
-config.font = wezterm.font("JetBrainsMonoNL Nerd Font")
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 
 -- and finally, return the configuration to wezterm
 return config
