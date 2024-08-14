79fd28ecacac3f9194472b0983d1747ac2411b12
Tony Majestro <tonymajestro@gmail.com>
2024-08-13 21:08:12 -0700

    Add trouble config

---
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,20 @@
+return {
+  "folke/trouble.nvim",
+  keys = {
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle focus=true<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>xX",
+      "<cmd>Trouble diagnostics focus=true toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+  },
+  opts = {
+    mapping = {
+      n = { ["q"] = "TroubleToggle" },
+    },
+  },
+}
