d8885cd169d3eb8a2d63df16440753fc2b85789d
Tony Majestro <tonymajestro@gmail.com>
2024-08-18 21:49:24 -0700

    Use undo tree

--- nvim/.config/nvim/lua/plugins/telescope-undo.lua
+++ nvim/.config/nvim/lua/plugins/telescope-undo.lua
@@ -1,49 +1,7 @@
 return {
-  "debugloop/telescope-undo.nvim",
-  dependencies = { -- note how they're inverted to above example
-    {
-      "nvim-telescope/telescope.nvim",
-      dependencies = { "nvim-lua/plenary.nvim" },
-    },
-  },
+  "mbbill/undotree",
   keys = {
-    {
-      "<leader>uu",
-      "<cmd>Telescope undo<cr>",
-      desc = "undo history",
-    },
+    { "<leader>uu", "<cmd>UndotreeToggle<cr><cmd>UndotreeFocus<cr>", desc = "Undotree toggle" },
+    { "<esc>", "<cmd>UndotreeToggle<cr>", desc = "Undotree toggle", ft = "undotree" },
   },
-  opts = function()
-    return {
-      extensions = {
-        undo = {
-          use_delta = false,
-          vim_diff_opts = {
-            ctxlen = vim.o.scrolloff,
-          },
-          layout_config = {
-            preview_width = 0.7,
-            preview_cutoff = 0,
-            width = 0.99,
-            height = 0.99,
-          },
-          mappings = {
-            i = {
-              ["<cr>"] = require("telescope-undo.actions").restore,
-            },
-            n = {
-              ["<cr>"] = require("telescope-undo.actions").restore,
-            },
-          },
-        },
-      },
-    }
-  end,
-  config = function(_, opts)
-    -- Calling telescope's setup from multiple specs does not hurt, it will happily merge the
-    -- configs for us. We won't use data, as everything is in it's own namespace (telescope
-    -- defaults, as well as each extension).
-    require("telescope").setup(opts)
-    require("telescope").load_extension("undo")
-  end,
 }
