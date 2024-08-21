6e89960ca05731bf2da97a1c493eb8d03153c0b0
Tony Majestro <majestro@amazon.com>
2024-08-21 11:11:50 -0700

    Swap local and global telescope keybindings

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -35,6 +35,10 @@ return {
   end,
   keys = {
     { "<leader>fg", "<cmd>Telescope git_status<CR>", desc = "Telescope git status" },
+    { "<leader>ff", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
+    { "<leader>fF", LazyVim.pick("files"), desc = "Find Files (Root Dir)" },
+    { "<leader>sG", LazyVim.pick("live_grep"), desc = "Grep (Root Dir)" },
+    { "<leader>sg", LazyVim.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
     {
       "<leader><space>",
       "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>",
