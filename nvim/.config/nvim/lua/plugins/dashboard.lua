4b8dd61cb1d3b29516fb5daecd0e67f1f7204ec2
Tony Majestro <majestro@amazon.com>
2024-08-21 23:04:30 -0700

    Update dashboard.nvim

--- nvim/.config/nvim/lua/plugins/dashboard.lua
+++ nvim/.config/nvim/lua/plugins/dashboard.lua
@@ -1,57 +1,62 @@
+local function getHeader()
+  local header = {}
+
+  local lines = 18
+  local left = vim.o.lines - lines
+  local whitespaceLines = left / 2
+
+  for _ = 1, whitespaceLines do
+    table.insert(header, "")
+  end
+  table.insert(header, "Neovim")
+  table.insert(header, "")
+
+  return header
+end
+
+local function getCenter()
+  return {
+    {
+      action = "lua LazyVim.pick()()",
+      desc = " Find File",
+      key = "f",
+    },
+    {
+      action = "ene | startinsert",
+      desc = " New File",
+      key = "n",
+    },
+    {
+      action = "Neotree",
+      desc = " File Explorer",
+      key = "e",
+    },
+    {
+      action = 'lua require("persistence").load()',
+      desc = " Restore Session",
+      key = "s",
+    },
+    {
+      action = "Lazy",
+      desc = " Lazy",
+      key = "l",
+    },
+    {
+      action = function()
+        vim.api.nvim_input("<cmd>qa<cr>")
+      end,
+      desc = " Quit",
+      key = "q",
+    },
+  }
+end
+
 return {
   "nvimdev/dashboard-nvim",
   opts = {
     config = {
-      center = {
-        {
-          action = "lua LazyVim.pick()()",
-          desc = " Find File",
-          key = "f",
-        },
-        {
-          action = "ene | startinsert",
-          desc = " New File",
-          key = "n",
-        },
-        {
-          action = "Neotree",
-          desc = " File Explorer",
-          key = "e",
-        },
-        {
-          action = 'lua require("persistence").load()',
-          desc = " Restore Session",
-          key = "s",
-        },
-        {
-          action = "Lazy",
-          desc = " Lazy",
-          key = "l",
-        },
-        {
-          action = function()
-            vim.api.nvim_input("<cmd>qa<cr>")
-          end,
-          desc = " Quit",
-          key = "q",
-        },
-      },
-      header = {
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "Neovim",
-        "",
-        "",
-      },
-      footer = {},
+      header = getHeader(),
+      center = getCenter(),
     },
   },
 }
