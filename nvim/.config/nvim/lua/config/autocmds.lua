56c1265f92418ceadbe04efc6da94aefa08c05aa
Tony Majestro <majestro@amazon.com>
2024-08-27 22:34:58 -0700

    Use treesitter syntax highlighting instead of LSP highlighting

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -36,3 +36,10 @@ vim.api.nvim_create_autocmd("FileType", {
     })
   end,
 })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    client.server_capabilities.semanticTokensProvider = nil
+  end,
+})
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,9 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    highlight = {
-      enable = false,
-    },
     auto_install = true,
     ensure_installed = {
       "java",
