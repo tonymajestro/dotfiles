1c26c28f9408a9ca1d0cbf04751beb2705efeb56
Tony Majestro <majestro@amazon.com>
2024-08-30 10:55:51 -0700

    Add harpoon, nvim-cmp keymaps

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -16,6 +16,8 @@ return {
         ["<C-p>"] = cmp.mapping.select_prev_item(),
         ["<C-n>"] = cmp.mapping.select_next_item(),
         ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<C-Space>"] = cmp.mapping.complete(),
       }),
     }
   end,
