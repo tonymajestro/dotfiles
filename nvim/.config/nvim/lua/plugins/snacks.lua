132e2113f6be00e9f10e26d6f341a84060511f16
Tony Majestro <tonymajestro@gmail.com>
2024-12-11 23:06:14 -0800

    Remove yazi from dashboard

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -7,12 +7,6 @@ return {
         keys = {
           { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
           { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-          {
-            icon = "󰔱",
-            key = "e",
-            desc = "Open file explorer",
-            action = "<cmd>Yazi<cr>",
-          },
           { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
           { icon = " ", key = "s", desc = "Restore Session", section = "session" },
           { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
