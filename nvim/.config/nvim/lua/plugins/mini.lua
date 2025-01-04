eb89b918781f60b8eb2f32ab28d034c2c7db0f61
Tony Majestro <tonymajestro@gmail.com>
2025-01-04 14:57:14 -0800

    Add mini.ai configuration

---
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,11 @@
+return {
+  "echasnovski/mini.ai",
+  keys = {
+    -- Ignore user prompt keybindings
+    { "da?", "<nop>" },
+    { "di?", "<nop>" },
+  },
+  opts = {
+    silent = true,
+  },
+}
