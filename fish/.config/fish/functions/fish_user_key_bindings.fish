969e78697723464fe89dc916841d0fe3655061cb
Tony Majestro <tonymajestro@gmail.com>
2025-01-04 17:23:36 -0800

    Add fish keybinding for zd

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -6,4 +6,6 @@ function fish_user_key_bindings
 
     bind -M insert -m default kj cancel repaint-mode
     set -g fish_sequence_key_delay_ms 150
+
+    bind \cz --mode insert 'cd (zoxide query --interactive); commandline -f repaint'
 end
