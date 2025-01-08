c3c007f3d86c531a7c82d71864195ab9041d4b4d
Tony Majestro <tonymajestro@gmail.com>
2025-01-08 10:23:55 -0800

    Update keymaps for fzf-lua pick recent files

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -3,6 +3,8 @@ return {
   opts = {},
   keys = {
     { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
+    { "<leader>fr", LazyVim.pick("oldfiles", { cwd = vim.uv.cwd() }), desc = "Recent (cwd)" },
+    { "<leader>fR", "<cmd>FzfLua oldfiles<cr>", desc = "Recent" },
     { "<leader>ff", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
     { "<leader>fF", LazyVim.pick("files"), desc = "Find Files (Root Dir)" },
   },
