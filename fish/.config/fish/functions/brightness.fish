af66d868edfa73293912e49ef5618a2c9abc28ce
Tony Majestro <tonymajestro@gmail.com>
2025-01-27 18:35:54 -0800

    fish: update fzf default opts and brightness util function to print current brightness if there are no args

--- fish/.config/fish/functions/brightness.fish
+++ fish/.config/fish/functions/brightness.fish
@@ -1,3 +1,8 @@
 function brightness
-    ddcutil setvcp 10 $argv &>/dev/null
+    if test (count $argv) -eq 0
+        ddcutil getvcp 10 2>/dev/null | awk '{ print substr($9, 1, length($9)-1) }'
+    else
+        ddcutil setvcp 10 $argv &>/dev/null
+    end
+
 end
