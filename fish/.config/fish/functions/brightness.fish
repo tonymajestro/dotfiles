123754517215c42d7b71d8a5a1557a59b239685e
Tony Majestro <tonymajestro@gmail.com>
2025-01-29 21:18:40 -0800

    fish: add --brief and --noverify flags to ddcutil to speed up brightness function

--- fish/.config/fish/functions/brightness.fish
+++ fish/.config/fish/functions/brightness.fish
@@ -1,8 +1,8 @@
 function brightness
     if test (count $argv) -eq 0
-        ddcutil getvcp 10 --bus 4 2>/dev/null | awk '{ print substr($9, 1, length($9)-1) }'
+        ddcutil getvcp 10 --bus 4 --brief 2>/dev/null | awk '{ print $4 }'
     else
-        ddcutil setvcp 10 $argv --bus 4 &>/dev/null
+        ddcutil setvcp 10 $argv --bus 4 --noverify &>/dev/null
     end
 
 end
