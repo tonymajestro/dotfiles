c5f0d15ee6563f1e5fb52314ec2ad3cb48857bd6
Tony Majestro <tonymajestro@gmail.com>
2025-01-29 17:49:44 -0800

    fish: add --bus to ddcci brightness command

--- fish/.config/fish/functions/brightness.fish
+++ fish/.config/fish/functions/brightness.fish
@@ -1,8 +1,8 @@
 function brightness
     if test (count $argv) -eq 0
-        ddcutil getvcp 10 2>/dev/null | awk '{ print substr($9, 1, length($9)-1) }'
+        ddcutil getvcp 10 --bus 4 2>/dev/null | awk '{ print substr($9, 1, length($9)-1) }'
     else
-        ddcutil setvcp 10 $argv &>/dev/null
+        ddcutil setvcp 10 $argv --bus 4 &>/dev/null
     end
 
 end
