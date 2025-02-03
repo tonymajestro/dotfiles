9633d3bb002d1d61390ebd5871d8cce1e6b3c7c8
Tony Majestro <tonymajestro@gmail.com>
2025-02-03 14:27:04 -0800

    feat(fish): add keybinding for clearing terminal

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -3,6 +3,7 @@ function fish_user_key_bindings
     fish_vi_key_bindings --no-erase insert
 
     bind \co --mode insert 'y; commandline -f repaint'
+    bind \cj --mode insert 'clear; commandline -f repaint'
 
     bind -M insert -m default kj cancel repaint-mode
     set -g fish_sequence_key_delay_ms 150
