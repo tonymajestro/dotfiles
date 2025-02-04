662de564035316b60cb936c62db02ef848a5a6a6
Tony Majestro <tonymajestro@gmail.com>
2025-02-03 22:12:00 -0800

    feat(nvim): configure snacks.input to open above cursor

---
+++ nvim/.config/nvim/lua/plugins/input.lua
@@ -0,0 +1,44 @@
+local style = {
+  backdrop = false,
+  position = "float",
+  border = "rounded",
+  title_pos = "center",
+  height = 1,
+  width = 60,
+  noautocmd = true,
+  -- relative = "editor",
+  -- row = 2,
+  relative = "cursor",
+  row = -3,
+  col = 0,
+  wo = {
+    winhighlight = "NormalFloat:SnacksInputNormal,FloatBorder:SnacksInputBorder,FloatTitle:SnacksInputTitle",
+    cursorline = false,
+  },
+  bo = {
+    filetype = "snacks_input",
+    buftype = "prompt",
+  },
+  --- buffer local variables
+  b = {
+    completion = false, -- disable blink completions in input
+  },
+  keys = {
+    n_esc = { "<esc>", { "cmp_close", "cancel" }, mode = "n", expr = true },
+    i_esc = { "<esc>", { "cmp_close", "cancel" }, mode = "i", expr = true },
+    i_cr = { "<cr>", { "cmp_accept", "confirm" }, mode = "i", expr = true },
+    i_tab = { "<tab>", { "cmp_select_next", "cmp" }, mode = "i", expr = true },
+    i_ctrl_w = { "<c-w>", "<c-s-w>", mode = "i", expr = true },
+    i_up = { "<up>", { "hist_up" }, mode = { "i", "n" } },
+    i_down = { "<down>", { "hist_down" }, mode = { "i", "n" } },
+    q = "cancel",
+  },
+}
+return {
+  "folke/snacks.nvim",
+  opts = {
+    input = {
+      win = { style = style },
+    },
+  },
+}
