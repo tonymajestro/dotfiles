33fff16328e4072c6e513c16dc53e0153000568a
Tony Majestro <tonymajestro@gmail.com>
2025-02-06 19:07:59 -0800

    feat(htmlpretty): add support for xml parsing

--- bin/htmlpretty/htmlpretty.py
+++ bin/htmlpretty/htmlpretty.py
@@ -1,8 +1,32 @@
 from bs4 import BeautifulSoup
 import sys
 
-lines = sys.stdin.readlines()
-html = "".join(lines)
-soup = BeautifulSoup(html, "html.parser")
 
-print(soup.prettify())
+def getParser():
+    supportedParsers = {
+        "xml": "lxml",
+        "lxml": "lxml",
+        "html": "html.parser",
+        "html.parser": "html.parser",
+    }
+
+    if len(sys.argv) > 1:
+        parser = sys.argv[1]
+    else:
+        parser = "html.parser"
+
+    if parser not in supportedParsers:
+        print("Parser not supported: " + parser)
+        sys.exit(1)
+
+    return supportedParsers[parser]
+
+
+if __name__ == "__main__":
+    parser = getParser()
+
+    lines = sys.stdin.readlines()
+    html = "".join(lines)
+    soup = BeautifulSoup(html, parser)
+
+    print(soup.prettify())
