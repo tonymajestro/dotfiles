dbc39eae234bb9ec9e317ae6889e081be59066b4
Tony Majestro <tonymajestro@gmail.com>
2025-03-03 15:21:14 -0800

    feat(fish): remove vi mode

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,12 +1,6 @@
 function fish_user_key_bindings
-    fish_default_key_bindings -M insert
-    fish_vi_key_bindings --no-erase insert
+    bind \co 'y; commandline -f repaint'
+    bind \cj 'clear; commandline -f repaint'
 
-    bind \co --mode insert 'y; commandline -f repaint'
-    bind \cj --mode insert 'clear; commandline -f repaint'
-
-    bind -M insert -m default kj cancel repaint-mode
-    set -g fish_sequence_key_delay_ms 150
-
-    bind \cz --mode insert 'zd; commandline -f repaint'
+    bind \cz 'zd; commandline -f repaint'
 end
