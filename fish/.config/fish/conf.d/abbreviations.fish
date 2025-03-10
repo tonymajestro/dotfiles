f1e89879bfba61e1e2dff75605bebeeb8939ed9c
Tony Majestro <tonymajestro@gmail.com>
2025-03-09 17:54:23 -0700

    feat: remove zd function since zi does the same thing

--- fish/.config/fish/conf.d/abbreviations.fish
+++ fish/.config/fish/conf.d/abbreviations.fish
@@ -1,6 +1,7 @@
 abbr vim nvim
 abbr lg lazygit
 abbr fish_reload_config 'source ~/.config/fish/**/*.fish'
-abbr tree tree -C --gitignore
+abbr tree 'tree -C --gitignore'
 abbr pd prevd
 abbr nd nextd
+abbr untar 'tar -xzvf'
--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,4 @@
 function fish_user_key_bindings
     bind \co 'y; commandline -f repaint'
     bind \cj 'clear; commandline -f repaint'
-
-    bind \cz 'zd; commandline -f repaint'
 end
