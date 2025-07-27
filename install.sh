6dc3784fc886a9c502f123ec7ad671288f657e28
Tony Majestro <tonymajestro@gmail.com>
2025-07-26 22:55:34 -0700

    feat: remove .wezterm, add ghostty install script

--- install.sh
+++ install.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 set -e
 
-# Build reddittui
-echo "Building reddittui application..."
-mkdir -p "$BUILD_DIR"
-go build -o "$BUILD_DIR/$APP_NAME" "$GO_MAIN_FILE"
+GHOSTTY_DIR="ghostty/.config/ghostty"
+GHOSTTY_DEV_CONFIG_FILE="config.dev"
 
 # Detect OS type
 OS_TYPE="$(uname)"
 
+# Set ghostty fish shell
 if [[ "$OS_TYPE" == "Darwin" ]]; then
   echo "Running on macOS"
   COMMAND="/opt/homebrew/bin/fish --login --interactive"
 elif [[ "$OS_TYPE" == "Linux" ]]; then
-  COMMAND="/opt/homebrew/bin/fish --login --interactive"
+  COMMAND="/usr/bin/fish --login --interactive"
 else
   echo "Unknown OS: $OS_TYPE"
 fi
+
+echo "Writing ghostty config to $GHOSTTY_DIR/$GHOSTTY_DEV_CONFIG_FILE"
+echo "command = $COMMAND" >"$GHOSTTY_DIR/$GHOSTTY_DEV_CONFIG_FILE"
 
 echo "Installation complete."
