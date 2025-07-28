057e4a8acc415a65a08d0e931a54f22bc184801e
Tony Majestro <majestro@amazon.com>
2025-07-28 10:20:04 -0700

    chore(nvim): update lazy, remove yazi

--- nvim/.config/nvim/lua/plugins/yazi.lua
+++ nvim/.config/nvim/lua/plugins/yazi.lua
@@ -8,12 +8,6 @@ return {
       "<cmd>Yazi<cr>",
       desc = "Open yazi at the current file",
     },
-    {
-      -- Open in the current working directory
-      "<leader>E",
-      "<cmd>Yazi cwd<cr>",
-      desc = "Open the file manager in nvim's working directory",
-    },
   },
   opts = {
     open_for_directories = true,
