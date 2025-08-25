f322bd71fe212803daed2492966f071bc38b8101
Tony Majestro <majestro@amazon.com>
2025-08-24 19:42:25 -0700

    chore(nvim): clean up mini highlight groups

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -3,6 +3,7 @@ return {
     'echasnovski/mini.ai',
     version = '*',
     config = function()
+
       -- https://github.com/LazyVim/LazyVim/blob/25abbf546d564dc484cf903804661ba12de45507/lua/lazyvim/util/mini.lua#L5
       local function ai_buffer(ai_type)
         local start_line, end_line = 1, vim.fn.line("$")
@@ -25,6 +26,48 @@ return {
           g = ai_buffer
         }
       })
+    end
+  },
+  {
+    'echasnovski/mini.tabline',
+    version = '*',
+    config = function()
+      require("mini.tabline").setup({
+        show_icons = false
+      })
+
+      local colors = require("catppuccin.palettes.macchiato")
+
+      -- current tab
+      vim.api.nvim_set_hl(0, "MiniTablineCurrent", {
+        fg = colors.text,
+        bg = colors.base,
+        sp = colors.red,
+        bold = true,
+        underline = true,
+      })
+
+      -- current tab modified
+      vim.api.nvim_set_hl(0, "MiniTablineModifiedCurrent", {
+        fg = colors.red,
+        bg = colors.base,
+        sp = colors.red,
+        bold = true,
+        underline = true,
+      })
+
+      -- other tabs
+      vim.api.nvim_set_hl(0, "MiniTablineHidden", {
+        fg = colors.overlay1,
+        bg = colors.crust
+      })
+
+      -- other tabs modified
+      vim.api.nvim_set_hl(0, "MiniTablineModifiedHidden", {
+        fg = colors.red,
+        bg = colors.crust,
+      })
+
     end
   },
 }
