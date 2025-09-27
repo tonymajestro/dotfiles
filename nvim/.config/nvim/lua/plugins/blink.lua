e00424fa06daada85961e61e30e7719ad4b17b5d
Tony Majestro <majestro@amazon.com>
2025-09-27 02:38:28 -0700

    feat(nvim): revert blink updatems change

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -19,7 +19,7 @@ return {
       },
       menu = {
         auto_show = true,
-        auto_show_delay_ms = 350,
+        auto_show_delay_ms = 300,
         draw = {
           treesitter = { "lsp" },
         },
